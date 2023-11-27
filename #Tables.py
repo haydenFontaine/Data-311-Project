ca0c8ee13149bd754eb54a84c3fb2e663c0e4e6d
MarcusMartindale <152224056+MarcusMartindale@users.noreply.github.com>
2023-11-27 15:01:47 -0700

    Update #Tables.py
    5 tables created

--- #Tables.py
+++ #Tables.py
@@ -1,10 +1,12 @@
 import sqlite3
 
+
+
 conn = sqlite3.connect("Table1")
 
 conn.execute('''
-     CREATE TABLE LoginINFO
-     (ID    INT PRIMARY KEY,
+     CREATE TABLE LOGININFO
+     (USERID    INT PRIMARY KEY,
      USERNAME   TEXT UNIQUE,
      PASSWORD   TEXT,
      FULLNAME   TEXT,
@@ -13,4 +15,57 @@ conn.execute('''
      REGDATE    DATE); 
     ''')
 conn.commit()
+
+
+
+conn = sqlite3.connect("Table2")
+
+conn.execute('''
+     CREATE TABLE TWEETS
+     (TWEETID   INT PRIMARY KEY UNIQUE,
+      USERID    INT,
+      TWEET TEXT,
+      TWEETTIME     DATE); 
+    ''')
+conn.commit()
+
+
+
+conn = sqlite3.connect("Table3")
+
+conn.execute('''
+     CREATE TABLE FOLLOWT
+     (FOLLOWID   INT PRIMARY KEY UNIQUE,
+      FOLLOWERUSERID    INT,
+      FOLLOWINGUSERID    INT); 
+    ''')
+conn.commit()
+
+
+
+conn = sqlite3.connect("Table4")
+
+conn.execute('''
+     CREATE TABLE LIKE
+     (LIKEID   INT PRIMARY KEY UNIQUE,
+      LIKEUSERID INT,
+      TWEETID  TEXT); 
+    ''')
+conn.commit()
+
+
+
+conn = sqlite3.connect("Table5")
+
+conn.execute('''
+     CREATE TABLE COMMENT
+     (COMMENTID   INT PRIMARY KEY UNIQUE,
+      COMMENTUSERID INT,
+      TWEETID  TEXT,
+      COMMENT   TEXT,
+      COMMENTTIME   DATE); 
+    ''')
+conn.commit()
+
+
 conn.close()
